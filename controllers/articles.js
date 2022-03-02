80e9c60a2ac6cfc16a97525d3bccfe0f137ba5c3
chillyweather <d.dzmitryeu@gmail.com>
2022-03-02 17:30:26 +0200

    test

--- controllers/articles.js
+++ controllers/articles.js
@@ -35,6 +35,7 @@ module.exports.createArticle = (req, res, next) => {
 
 //  delete article
 module.exports.deleteArticle = (req, res, next) => {
+  console.log(req.params);
   Article.findById(req.params.id)
     .orFail()
     .then((article) => {
