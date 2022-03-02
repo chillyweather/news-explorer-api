fb0450e0f57cc773fba16d9de71c8c4b755c3243
chillyweather <d.dzmitryeu@gmail.com>
2022-03-02 18:04:11 +0200

    test

--- controllers/articles.js
+++ controllers/articles.js
@@ -36,7 +36,7 @@ module.exports.createArticle = (req, res, next) => {
 //  delete article
 module.exports.deleteArticle = (req, res, next) => {
   res.send(req.params);
-  Article.findById(req.params._id)
+  Article.findById(req.params.id)
     .orFail()
     .then((article) => {
       if (req.user._id === article.owner._id.toString()) {
